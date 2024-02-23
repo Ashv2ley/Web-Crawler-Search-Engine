91ba5b363b15e6dd0bd1e4849bf2d8099549f1f3
Antonio Edwards <130106384+aedwardz@users.noreply.github.com>
2024-02-23 00:26:09 -0800

    Update InvertedIndex.py
    wrote a function, readZip that reads the contents of a zipped file

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -1,12 +1,34 @@
-from nltk.stem import PorterStemmer
-import re
+from dataclasses import dataclass
+import zipfile
 
-userSearch = input('Search: ')
+@dataclass
+class indexStats():
+    """
+    Class that keeps track of all the stats of the index
+    """
+    numDocs: int = 0
+    uniquePages: int = 0
+    totalSize: int = 0
 
-tokens = re.findall(r'\w+', userSearch.lower(), flags=re.ASCII)
 
-ps = PorterStemmer()
-stemmedTokens = list(map(lambda token: ps.stem(token), tokens))
+def readZip(path:str):
+    """
+    Takes in a path to a zip file, and reads its contents
+    :param path:
+        path to the zip file
+    :return: None
+    """
+    with zipfile.ZipFile(path, 'r') as zip_file:
+        file_list = zip_file.namelist()
+        for file_name in file_list:
+            with zip_file.open(file_name) as file:
+                stats.numDocs+=1
 
 
-print(stemmedTokens)
+if __name__ == "__main__":
+    stats = indexStats()
+    readZip("developer.zip")
+
+    print(stats.numDocs)
+
+
